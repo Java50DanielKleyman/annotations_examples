8288b69bb8c021ed7f8f935fcff1fe47c48a71de
Daniel Kleyman <denny_klm1@yahoo.com>
2023-12-03 16:06:04 +0200

    HW-50

--- src/main/java/telran/test/TestLibrary.java
+++ src/main/java/telran/test/TestLibrary.java
@@ -1,5 +1,6 @@
 package telran.test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import telran.reflect.BeforeEach;
@@ -8,25 +9,25 @@ import telran.reflect.Test;
 public class TestLibrary {
 	public static void launchTest(Object testObj) throws Exception {
 		Method[] methods = testObj.getClass().getDeclaredMethods();
-		Method beforeEachMethod = getBeforeEachMethod(methods);
 		for (Method method : methods) {
 			if (method.isAnnotationPresent(Test.class)) {
-				beforeEachMethod.invoke(testObj);
+				runBeforeEachMethods(testObj, methods);
 				method.setAccessible(true);
 				method.invoke(testObj);
 			}
 		}
 	}
-	
-	private static Method getBeforeEachMethod(Method[] methods) {
-		Method beforeEachMethod = null;
+
+	private static void runBeforeEachMethods(Object testObj, Method[] methods)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
 		for (Method method : methods) {
 			if (method.isAnnotationPresent(BeforeEach.class)) {
-				beforeEachMethod = method;
-				beforeEachMethod.setAccessible(true);
-				break;
+				method.setAccessible(true);
+				method.invoke(testObj);
+				;
 			}
 		}
-		return beforeEachMethod;
+
 	}
 }
