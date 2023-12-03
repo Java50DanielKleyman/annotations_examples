1bbd5fd2e5bebc6a2e31fef74c00f8c359b99124
Daniel Kleyman <denny_klm1@yahoo.com>
2023-12-03 14:45:00 +0200

    HW-50

---
+++ src/main/java/telran/test/TestLibrary.java
@@ -0,0 +1,31 @@
+package telran.test;
+
+import java.lang.reflect.Method;
+
+import telran.reflect.BeforeEach;
+import telran.reflect.Test;
+
+public class TestLibrary {
+	public static void launchTest(Object testObj) throws Exception {
+		Method[] methods = testObj.getClass().getDeclaredMethods();
+		Method beforeEachMethod = getBeforeEachMethod(methods);
+		for (Method method : methods) {
+			if (method.isAnnotationPresent(Test.class)) {
+				
+				method.setAccessible(true);
+				method.invoke(testObj);
+			}
+		}
+	}
+
+	private static Method getBeforeEachMethod(Method[] methods) {
+		Method beforeEachMethod = null;
+		for (Method method : methods) {
+			if (method.isAnnotationPresent(BeforeEach.class)) {
+				method.setAccessible(true);
+				break;
+			}
+		}
+		return beforeEachMethod;
+	}
+}
